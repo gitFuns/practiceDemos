4217356be5bf23a636aba9d503bce3b4720e5094
joshua <wanghaibo20081815@163.com>
2016-12-16 17:01:58 +0800

    阿里的移动端适配方案 -- https://github.com/amfe/lib-flexible.git

---
+++ canvas专题/index.js
@@ -0,0 +1,11 @@
+var cvs = document.querySelector('#cvs');
+var ctx = cvs.getContext('2d');
+
+/*step 1*/
+// ctx.moveTo(50, 50);
+// ctx.lineTo(200, 200);
+// ctx.stroke();
+
+/*step 2*/
+// ctx.fillStyle = 'green';
+// ctx.fillRect(110, 110, 200, 200);
